b4e6e764cbf6467caab66ca5455cdc53ed74e535
gabriel-senan <gabrielsnnh50@gmail.com>
2024-11-27 23:13:12 -0300

    rotas e controller

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -5,6 +5,13 @@ use \core\Controller;
 
 class HomeController extends Controller {
 
+    private $loggedUser;
+
+    public function __construct()
+    {
+        $this->redirect('/login');
+    }
+
     public function index() {
         $this->render('home', ['nome' => 'Bonieky']);
     }
