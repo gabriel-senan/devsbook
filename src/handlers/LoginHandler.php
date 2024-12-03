617589e8dff9183aa880b48ef4b2e445ca686693
gabriel-senan <gabrielsnnh50@gmail.com>
2024-12-03 15:09:24 -0300

    user

--- src/handlers/LoginHandler.php
+++ src/handlers/LoginHandler.php
@@ -1,15 +1,18 @@
 <?php
+
 namespace src\handlers;
 
 use \src\models\User;
 
-class LoginHandler {
-    public static function checkLogin() {
-        if(!empty($_SESSION['token'])) {
+class LoginHandler
+{
+    public static function checkLogin()
+    {
+        if (!empty($_SESSION['token'])) {
             $token = $_SESSION['token'];
 
             $data = User::select()->where('token', $token)->one();
-            if(count($data) > 0) {
+            if (count($data) > 0) {
 
                 $loggedUser = new User();
                 $loggedUser->setID($data['id']);
